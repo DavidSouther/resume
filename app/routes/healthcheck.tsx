b148d48c4da6f154d154561fc914b040e66d549c
David Souther <davidsouther+github@gmail.com>
2023-03-28 22:39:30 +0200

    Movex nextjs to top

--- app/routes/healthcheck.tsx
+++
@@ -1,8 +0,0 @@
-export async function loader() {
-  try {
-    return new Response("OK");
-  } catch (error: unknown) {
-    console.log("healthcheck ❌", { error });
-    return new Response("ERROR", { status: 500 });
-  }
-}
--- nextjs/src/components/P.tsx
+++
@@ -1,5 +0,0 @@
-import Markdown from "marked-react";
-
-export const P = ({ children }: { children: string }) => (
-  <Markdown>{children}</Markdown>
-);
