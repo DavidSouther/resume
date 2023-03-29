eb9fa29cd3d9f4817987e829077e711c7c6fe402
David Souther <davidsouther+github@gmail.com>
2023-03-29 06:36:10 +0200

    Build

--- docs/_next/static/0woDmF8BhlqmVZEc1v71y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/154-1d1cbd6369867f8f.js","static/chunks/pages/index-32060b12ce9c777c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
