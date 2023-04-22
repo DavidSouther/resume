4df3b93403b8a409c3be346891cb772afef5f60b
David Souther <davidsouther+github@gmail.com>
2023-04-21 23:33:31 -0700

    Add manifest to make installable

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,17 @@ export default function Document() {
           rel="stylesheet"
           href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css"
         />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="white"
+        />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: dark)"
+          content="black"
+        />
       </Head>
       <body className="container">
         <Main />
