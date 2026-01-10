e05c796a94ec231a937d19f31f94a67ca5a0c33f
David Souther <david.souther+github@gmail.com>
2026-01-10 11:43:24 -0500

    Update Annie photos

--- src/app/annie_40/page.tsx
+++ src/app/annie_40/page.tsx
@@ -1,50 +1,63 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import "./page.css";
 
 const URLS = [
     "grelli.jpg",
     "walk.jpg",
-    "pants.jpg"
+    "pants.jpg",
+    "krummi.jpg",
+    "cover.jpg",
+    "puffin.jpg",
+    "pumpkins.jpg",
 ];
 
-export default function Page() {
+function useRandom<T>(items: ReadonlyArray<T>, duration: number) {
+    const getItem = () => items[(Math.random() * items.length)|0];
+    const [item, dispatch] = useReducer(getItem, getItem());
+
     useEffect(() => {
         let canceled = false;
 
-        function changePhoto() {
+        function next() {
             if (canceled) return;
-            const idx = Math.floor(Math.random() * URLS.length);
-            const url = URLS[idx];
-            document.body.style.backgroundImage = `url(/annie_40/${url})`;
-            window.setTimeout(changePhoto, 10_000);
+            dispatch();
+            window.setTimeout(next, duration);
         }
-        window.setTimeout(changePhoto, 10_000);
+        window.setTimeout(next, duration);
 
         return () => {
             canceled = true;
-            document.body.style.backgroundImage = ``;
         }
+    }, [items, dispatch]);
 
-    });
+    return item;
+}
 
-    return <div className="container">
-        <h1>Save the Date!</h1>
-        <div className="subtitle">Annie's 40th Birthday</div>
-        
-        <div className="divider"></div>
-        
-        <div className="details">
-            <p><span className="highlight">Saturday, March 28th, 2026</span></p>
-            <p>Midtown, Manhattan</p>
-            <p>Location TBA</p>
-            <p>Hotel Block TBA</p>
-        </div>
-        
-        <div className="divider"></div>
-        
-        <div className="rsvp">
-            Tentative RSVP with David or Annie by Feb 20th
-        </div>
+
+export default function Page() {
+    const url = useRandom(URLS, 10_000);
+
+    return <div className="annie_40">
+        <div className="background" style={{'backgroundImage': `url("/annie_40/${url}")`}}></div>
+        <article>
+            <h1>Save the Date!</h1>
+            <div className="subtitle">Annie's 40th Birthday</div>
+            
+            <div className="divider"></div>
+            
+            <div className="details">
+                <p>Saturday, March 28th, 2026</p>
+                <p>Midtown, Manhattan</p>
+                <p>Location TBA</p>
+                <p>Hotel Block TBA</p>
+            </div>
+            
+            <div className="divider"></div>
+            
+            <div className="rsvp">
+                Tentative RSVP with David or Annie by Feb 20th
+            </div>
+        </article>
     </div>
 }
