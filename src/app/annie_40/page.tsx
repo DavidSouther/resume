3cfb97e1ec22b829b4029e6757f7e4002ca8304b
David Souther <david.souther+github@gmail.com>
2026-03-28 11:23:12 -0400

    Annie 40 arrival + playlist

--- src/app/annie_40/page.tsx
+++ src/app/annie_40/page.tsx
@@ -14,10 +14,10 @@ const URLS = [
 
 export async function generateMetadata(): Promise<Metadata> {
 	return {
-		title: `Annie's 40th Save the Date`,
+		title: `Annie's 40th birthday`,
 		metadataBase: new URL("https://davidsouther.com"),
 		openGraph: {
-			description: `Save the Date! Annie's 40th Birthday
+			description: `Annie's 40th Birthday
         Saturday, March 28th, 2026
 		1234 Broadway #147
         Hotel: Kimpton Eventi,
@@ -37,8 +37,30 @@ export default function Page() {
 		<div className="annie_40">
 			<Background urls={URLS} />
 			<article>
-				<h1>Save the Date!</h1>
-				<div className="subtitle">Annie's 40th Birthday</div>
+				<h1>Annie's 40th</h1>
+
+				<div className="details">
+					<p>Arrival Instructions</p>
+					<p>NEST Broadway 1234 Broadway #147 New York 10001</p>
+					<p>First, Buzz unit 147</p>
+					<p>Give us a minute to buzz you through?</p>
+					<p>Then try ButterflyMX code 349105</p>
+					<p>
+						If that doesn't work{" "}
+						<a href="tel:+1-650-595-5402">text or call David (650) 495-5402</a>
+					</p>
+				</div>
+
+				<div className="divider"></div>
+
+				<div className="playlist">
+					<iframe
+						title="Annie's 40th Playlist"
+						allow="autoplay *; encrypted-media *;"
+						sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+						src="https://embed.music.apple.com/us/playlist/40th-birthday/pl.u-06oxp9gFJ3XAG6"
+					></iframe>
+				</div>
 
 				<div className="divider"></div>
 
@@ -47,20 +69,14 @@ export default function Page() {
 					<p>6:30pm 1234 Broadway #147</p>
 					<p>Food, soft drinks, & music provided; BYOB</p>
 					<p>Dress code: Millenial Dance Party / that thing you never wear</p>
-					<p>Afterparty: <a href="https://www.hoteleventi.com/chelsea-bars/">Kimpton Eventi Back Bar</a></p>
+					<p>
+						Afterparty:{" "}
+						<a href="https://www.hoteleventi.com/chelsea-bars/">
+							Kimpton Eventi Back Bar
+						</a>
+					</p>
 				</div>
 
-				<div className="divider"></div>
-
-				<div className="rsvp">
-					<p>RSVP with David or Annie by March 21st</p>
-					<ul>
-						<li>Hotel Block Booking Links:</li>
-						<li><a href="https://www.ihg.com/redirect?path=asearch&brandCode=KI&localeCode=en&regionCode=1&hotelCode=NYCAA&checkInDate=27&checkInMonthYear=022026&checkOutDate=29&checkOutMonthYear=022026&rateCode=6CBARC&_PMID=99801505&GPC=EUF&cn=no&adjustMonth=false&showApp=true&monthIndex=00">Kimpton Eventi $299</a></li>
-						<li><a href="https://app.marriott.com/reslink?id=1770236208872&key=GRP&app=resvlink">Moxy NYC Chelsea $249</a></li>
-						<li>Last day to book: Wednesday, February 25, 2026</li>
-					</ul>
-				</div>
 			</article>
 		</div>
 	);
